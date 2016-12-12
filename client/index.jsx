5f46f7f2489b4c090ee3e0fd747820180a73ecb2
Mitchell Deane <mitchelld20@yahoo.com>
2016-12-12 04:21:45 -0600

    browserify is working and react is functionally rendering to the page

--- client/index.jsx
+++ client/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-ReactDOM.render( <App />, getDocumentById("app"));
+ReactDOM.render( <h1>I am rendering from react</h1>, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -11,7 +11,19 @@ app.use(express.static(path.join(__dirname, '/../client/')))
 app.use (bodyParser.json());
 
 app.get('/app-bundle.js',
-	browserify('./client/components/App.jsx', {
+	browserify('./client/index.jsx', {
 		transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
 	})
 )
+
+
+ app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../index.html'));
+ });
+
+var port = process.env.PORT || 4000;
+
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
