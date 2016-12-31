be8cb759b15013f5a03e887e46a10d4fec7ac2d6
GMDIV <mitchelld20@yahoo.com>
2016-12-30 18:07:35 -0600

    built out general react shell for main react app component

---
+++ client/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; 
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			
+
+		}
+	}
+
+	render() {
+
+		return (
+			<div>
+
+
+			</div>
+		)
+	}
+
+}
